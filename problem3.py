39cfe48f445cc1ed849d7ce26e29138ac01d6688
Gloxinia5 <punciaaron09@gmail.com>
2022-02-02 13:17:50 +0800

    Optimiziation

--- problem3.py
+++ problem3.py
@@ -1,39 +1,23 @@
-alpha_order = "abcdefghijklmnopqrstuvwxyz"
-s = 'asldkfjlaskdjf'
-curr_string = ""
+max_len = 0
+s = 'ujiedfjfrbhzamdwjjpgepd'
+curr_string = s[0]
 substrings = []
-char_index = 0
-
 for i in range(len(s)):
-    if i < len(s)-1:
-        if alpha_order.index(s[i]) >= char_index and not(alpha_order.index(s[i]) > alpha_order.index(s[i + 1])):
-            char_index = alpha_order.index(s[i])
+    if i != 0 and i < len(s) - 1:
+        if curr_string[-1] <= s[i]:
             curr_string += s[i]
-        elif alpha_order.index(s[i + 1]) < alpha_order.index(s[i]):
-            char_index = 0
-            curr_string += s[i]
+        elif curr_string[-1] > s[i]:
             substrings.append(curr_string)
-            curr_string = ""
-        else:
-            char_index = 0
-            substrings.append(curr_string)
-            curr_string = ""
+            curr_string = s[i]
+    elif i == 0:
+        continue
+    elif i == len(s) - 1 and s[-1] < curr_string[-1]:
+        substrings.append(curr_string)
     else:
         curr_string += s[i]
         substrings.append(curr_string)
-        break
-long = substrings[0]
-for j in range(len(substrings)):
-
-    if j < len(substrings) - 1:
-        if len(long) < len(substrings[j]):
-            long = substrings[j]
-        else:
-            long = long
-    else:
-        if len(long) < len(substrings[j]):
-            long = substrings[j]
-        else:
-            long = long
-            break
-print("Longest substring in alphabetical order is: ", long)
+for word in substrings:
+    if len(word) > max_len:
+        max_len = len(word)
+        long_word = word
+print("Longest substring in alphabetical order is:", long_word)
